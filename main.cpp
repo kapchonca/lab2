fb5d7c0dc8ce304d0a8c82c71c180cde2fa73149
kapchonca <147101548+kapchonca@users.noreply.github.com>
2023-10-19 19:27:50 +0100

    added timer thread

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <thread>
+// #include <mutex>
+
+static bool s_Finished = false;
+static bool s_TimeIsOver = false;
+
+// static std::mutex terminalMutex;
 
 std::string getRandomLine() { 
     std::ifstream dataset("./dataset");
@@ -26,32 +34,76 @@ std::string getRandomLine() {
     return returnLine;
 }
 
-int main() {
-    initscr(); // инициализирует библиотеку ncurses и подготавливает терминал к работе
-    start_color(); // подключает поддержку цветов
-    // cbreak();
-    // noecho();
-    // keypad(stdscr, TRUE);
-    // nodelay(stdscr, TRUE);
+class Player {
 
-    // std::string input = "";
+public:
+    int healthMax = 100;
+    int healthPoints = 100;
+    float attackPoints = 1.0f;
+    int level = 1;
+    int xp = 0;
+    int xpLimit = 100;
+
+
+};
+
+class Monster {
+
+public:
+    int time = 60;
+};
+
+void timer(Monster monster) {
+
+    using namespace std::literals::chrono_literals;
+
+    int timePassed = 1;
+
+    while (!s_Finished and (monster.time - ((timePassed-1) / 100)) > 0) {
+        // std::lock_guard guard{ terminalMutex };
+        // terminalMutex.lock();
+        // terminalMutex.lock();
+        mvprintw(15,15,"TIME: %d\n", monster.time - ((timePassed-1) / 100));
+        // terminalMutex.unlock();
+        refresh();
+        timePassed++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    if (!s_Finished) {
+    s_TimeIsOver = true;
+    }
+}
+
+
+
+void gaming(std::string text, Player& player, Monster& monster) {
+
+    clear();
+
+    // Start the timer
+
     std::string letters = ""; // хранит введенные символы
     // std::string word = "";
-    std::string text = getRandomLine(); // получаем строку для слепой печати
     
     init_pair(1, COLOR_WHITE, 160); // инициализируем первую цветовую палитру
     init_pair(2, 242, COLOR_BLACK); // инициализируем вторую цветовую палитру
     
 
     printw("Press 'ESC' to quit.\nFor better experience, turn on fullscreen mode.\n");
+    printw("HP: %d/%d\n", player.healthPoints, player.healthMax); // вывод здоровья игрока
     printw("%s\n", text.c_str()); // вывод строки по которой слепопечатаем
     printw("Your line: ");
     refresh();
 
+    std::thread countdown(&timer, monster); // создаем второй поток для независимой работы таймера
+    countdown.detach();
+
     int ch, mistakenChar; //инициализация переменных, хранящих текущий символ и неверно введенный символ соответственно
     int textLength = std::strlen(text.c_str());
     int textIndex = 0;
-    while (textIndex < textLength) {
+    int duration = 0;
+    while ((textIndex < textLength) && (player.healthPoints > 0) && (!s_TimeIsOver)) {
         bool mistake; 
         ch = getch(); // хватаем введенный символ
 
@@ -61,44 +113,60 @@ int main() {
             } else if (ch != text[textIndex]) { // при неверном вводе сохраняем неверный символ
                 mistake = true;
                 mistakenChar = ch;
+                player.healthPoints-=10;
             } else { // иначе добавляем к уже набранной строке и сдвигаем индекс исходной строки на 1
                 letters += ch; // Append the character to the letters string
-                // word += ch;
                 textIndex++;
             }
-
-            clear();
-
-    printw("Press 'ESC' to quit.\nFor better experience, turn on fullscreen mode.\n");
-
-            attron(COLOR_PAIR(2)); // включение цветового паттерна
-
-            printw("%s", text.substr(0,textIndex).c_str()); //вывод уже введенных символов
-
-            attroff(COLOR_PAIR(2)); // его выключение
-
-            printw("%s\n", text.substr(textIndex, std::strlen(text.c_str())).c_str()); // вывод еше не введенных символов
-
-            printw("Your line: %s", letters.c_str()); // вывод введенных символов
-
-            if (mistake) { // при неверном вводе подсвечивает неверно введенный символ
-                attron(COLOR_PAIR(1)); 
-                printw("%c", ch);
-                attroff(COLOR_PAIR(1));
-
-                mistake = false;
-            } 
-            // printw("Input: %s\n", input.c_str());
         }
+        clear();
+
+        printw("Press 'ESC' to quit.\nFor better experience, turn on fullscreen mode.\n");
+        printw("HP: %d/%d\n", player.healthPoints, player.healthMax); // вывод здоровья игрока
+
+        attron(COLOR_PAIR(2)); // включение цветового паттерна
+
+        printw("%s", text.substr(0,textIndex).c_str()); //вывод уже введенных символов
+
+        attroff(COLOR_PAIR(2)); // его выключение
+
+        printw("%s\n", text.substr(textIndex, std::strlen(text.c_str())).c_str()); // вывод еше не введенных символов
+
+        printw("Your line: %s", letters.c_str()); // вывод введенных символов
+
+        if (mistake) { // при неверном вводе подсвечивает неверно введенный символ
+            attron(COLOR_PAIR(1)); 
+            printw("%c", ch);
+            attroff(COLOR_PAIR(1));
+
+            mistake = false;
+        } 
+
+        // printw("Input: %s\n", input.c_str());
 
         // Handle other real-time tasks here
     }
 
+    s_Finished = true;
+    // countdown.join();
+    
+}
+
+
+int main() {
+
+    initscr(); // инициализирует библиотеку ncurses и подготавливает терминал к работе
+    start_color(); // подключает поддержку цветов
+    Player player;
+    Monster monster;
+    while (player.healthPoints > 0 and !s_TimeIsOver) {
+        std::string text = getRandomLine(); // получаем строку для слепой печати
+        gaming(text, player, monster);
+        s_Finished = false;
+    }
+
     endwin(); // закрытие окошка
 
-    // if (!input.empty()) {
-    //     std::cout << "You typed: " << input << std::endl;
-    // }
 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <chrono>
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "\033[38;5;8;48;5;0m" << "logh" << "\033[0m";
+int main() {
+    // Start the timer
+    auto start = std::chrono::steady_clock::now();
+
+    // Simulate some work or code execution
+    for (int i = 0; i < 100000; i++) {
+        for (int j = 0; j < 10000; j++) {
+
+        }
+    }
+
+    // Stop the timer
+    auto end = std::chrono::steady_clock::now();
+
+    // Calculate the elapsed time in seconds
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    int i = elapsedTime.count();
+
+    std::cout << "Elapsed time: " << elapsedTime.count() << " seconds" << std::endl << i;
+
     return 0;
 }
