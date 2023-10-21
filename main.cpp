5e89675d527580efdd140cd6312bfe71feb58bfb
kapchonca <147101548+kapchonca@users.noreply.github.com>
2023-10-21 09:26:00 +0100

    fully reworked game window (sprites, statuses, text)

--- main.cpp
+++ main.cpp
@@ -5,41 +5,53 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
-#include <thread>
-// #include <mutex>
+#include <variant>
 
-static bool s_Finished = false;
 static bool s_TimeIsOver = false;
 
-// static std::mutex terminalMutex;
 
-std::string getRandomLine() { 
+std::string getRandomLine() {  // достает случайную строчку для печати из базы
     std::ifstream dataset("./dataset");
+
     if (!dataset.is_open()) {
         throw std::invalid_argument( "Couldn't open the file" );
     }
 
     std::string totalPresets;
-    getline(dataset, totalPresets);
-    int totalPresetsNum = std::stoi(totalPresets);
+    getline(dataset, totalPresets); // в первой строке файла записано кол-во строчек для выбора 
+    int totalPresetsNum = std::stoi(totalPresets); // конвертация в инт
     std::string returnLine;
 
-    srand(time(0));
-    int lineNum = rand() % totalPresetsNum + 1;
+    srand(time(0)); // обновляем сид функции рандома при помощи текущего времени
+    int lineNum = rand() % totalPresetsNum + 1; // т.к. ранд возвращает большие значения, берем остаток от деления
 
     for (int i = lineNum; i>0; i--) {
-        getline(dataset, returnLine);
+        getline(dataset, returnLine); // перебираем строки, пока не дойдем до выпавшей рандомом
     }
     
-    return returnLine;
-}
+    return returnLine; 
+}   
+
+// class gameManager {  надеюсь разобраться и в будущем перенести сюда менеджмент состояний игрока
+
+// public:
+//      void stateIdle() {
+
+//      }
+
+//      void stateBattle() {
+
+//      }
+// };
+
+
 
 class Player {
 
 public:
     int healthMax = 100;
     int healthPoints = 100;
-    float attackPoints = 1.0f;
+    int attackPoints = 10;
     int level = 1;
     int xp = 0;
     int xpLimit = 100;
@@ -51,78 +63,150 @@ class Monster {
 
 public:
     int time = 60;
+    int healthMax = 1000;
+    int healthPoints = 1000;
+    int attackPoints = 7;
+    std::string name = "AIRAT";
+    std::string spriteInd = "1";
+
 };
 
-void timer(Monster monster) {
+// class Goblin: public Monster { в будущем появятся классы монстров, пока проверял, все ли работает с дочерними
 
-    using namespace std::literals::chrono_literals;
+// };
 
-    int timePassed = 1;
+// class Dragon: public Monster {
 
-    while (!s_Finished and (monster.time - ((timePassed-1) / 100)) > 0) {
-        // std::lock_guard guard{ terminalMutex };
-        // terminalMutex.lock();
-        // terminalMutex.lock();
-        mvprintw(15,15,"TIME: %d\n", monster.time - ((timePassed-1) / 100));
-        // terminalMutex.unlock();
-        refresh();
-        timePassed++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+// };
+
+
+void printSprite(Monster monster) {
+
+    std::string address = "sprites/" + monster.spriteInd; // путь к файлу
+    std::ifstream file(address);
+    std::string artLength; 
+    getline(file, artLength); // достаем из начала файла длину спрайта в строчках
+    int artLengthNum = std::stoi(artLength);
+
+    std::string sprite[artLengthNum]; // массив для хранения самого спрайта монстра
+
+    for (int i = artLengthNum; i > 0; i--) {
+        getline(file, sprite[artLengthNum - i]); // считываем файл в массив построчно
     }
 
-    if (!s_Finished) {
-    s_TimeIsOver = true;
+    for (int i = 0; i < 24; i++) {
+        mvprintw(15 + i, 90, "%s", sprite[i].c_str()); // вывод спрайта на экран
     }
+
 }
 
+void interface(Player& player, Monster& monster) {
+
+    int maxY, maxX;
+    getmaxyx(stdscr, maxY, maxX); // получаем измерения окна
+
+    for (int x = 0; x < maxX; x++) {
+        mvaddch(0, x, ACS_HLINE); // горизонтальная линия сверху
+    }
+
+    for (int x = 0; x < maxX; x++) {
+        mvaddch(maxY - 1, x, ACS_HLINE); // горизонтальная линия снизу
+    }
+
+    for (int y = 1; y < maxY - 1; y++) {
+        mvaddch(y, 0, ACS_VLINE); // вертикальные линии по бокам
+        mvaddch(y, maxX - 1, ACS_VLINE); 
+    }
+
+    for (int x = 1; x < maxX - 1; x++) {
+        mvaddch(maxY - 15, x, ACS_HLINE); // линия, отделяющая блок с текстом
+    }
+    for (int x = 1; x < maxX - 1; x++) {
+        mvaddch(11, x, ACS_HLINE); // линия, отделяющая блок со статусами
+    }
+
+    int centerY = maxY / 2;
+    int centerX = maxX / 2;
+
+    mvprintw(2, 118, "%s", monster.name.c_str()); // выводим все статусы
+    mvprintw(3, 111, "HP: %d/%d ATK: %d", monster.healthPoints, monster.healthMax, monster.attackPoints);
+    mvprintw(6, 119, "YOU");
+    mvprintw(7, 111, "HP: %d/%d ATK: %d", player.healthPoints, player.healthMax, player.attackPoints);
+    mvprintw(8, 112, "REMAINING TIME: ");
+    refresh(); 
+
+}
+
+// void stateIdle(Player& player, Monster& monster) { следующий шаг разработки
+//     interface(player, monster);
+// }
 
 
 void gaming(std::string text, Player& player, Monster& monster) {
 
     clear();
 
-    // Start the timer
 
     std::string letters = ""; // хранит введенные символы
-    // std::string word = "";
     
     init_pair(1, COLOR_WHITE, 160); // инициализируем первую цветовую палитру
     init_pair(2, 242, COLOR_BLACK); // инициализируем вторую цветовую палитру
     
+    interface(player, monster);
 
-    printw("Press 'ESC' to quit.\nFor better experience, turn on fullscreen mode.\n");
-    printw("HP: %d/%d\n", player.healthPoints, player.healthMax); // вывод здоровья игрока
-    printw("%s\n", text.c_str()); // вывод строки по которой слепопечатаем
-    printw("Your line: ");
-    refresh();
+    auto start = std::chrono::steady_clock::now(); // начало счетчика времени
 
-    std::thread countdown(&timer, monster); // создаем второй поток для независимой работы таймера
-    countdown.detach();
+    int ch; //инициализация переменных, хранящих текущий символ и неверно введенный символ соответственно
 
-    int ch, mistakenChar; //инициализация переменных, хранящих текущий символ и неверно введенный символ соответственно
+
     int textLength = std::strlen(text.c_str());
     int textIndex = 0;
     int duration = 0;
-    while ((textIndex < textLength) && (player.healthPoints > 0) && (!s_TimeIsOver)) {
+    int timePassed = 0;
+
+    mvprintw(48, 115, "YOUR LINE:");
+    refresh();
+
+
+    while ((textIndex < textLength) && (player.healthPoints > 0) && (monster.time - timePassed > 0) && (monster.healthPoints > 0)) {
+
+        auto end = std::chrono::steady_clock::now(); // каждый раз обновляем время
+
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+        timePassed = elapsedTime.count(); // приводит тип к инту
+
+        mvprintw(8, 128,"%d\n", monster.time - timePassed); // вывод времени
+        refresh();
+
         bool mistake; 
+
+        timeout(1000); // если в течение секунды пользователь не нажал кнопку, то скипаем getch() (нужно для корректной работы таймера)
+
         ch = getch(); // хватаем введенный символ
 
-        if (ch != ERR) {
+        if (ch != ERR) { // проверка, если кнопка все же была нажать
             if (ch == 27) { // выход на ESC
                 break;
             } else if (ch != text[textIndex]) { // при неверном вводе сохраняем неверный символ
                 mistake = true;
-                mistakenChar = ch;
-                player.healthPoints-=10;
+                // mistakenChar = ch;
+                player.healthPoints-=monster.attackPoints;
             } else { // иначе добавляем к уже набранной строке и сдвигаем индекс исходной строки на 1
                 letters += ch; // Append the character to the letters string
                 textIndex++;
+                monster.healthPoints-=player.attackPoints;
             }
         }
         clear();
 
-        printw("Press 'ESC' to quit.\nFor better experience, turn on fullscreen mode.\n");
-        printw("HP: %d/%d\n", player.healthPoints, player.healthMax); // вывод здоровья игрока
+        int maxY, maxX;
+        getmaxyx(stdscr, maxY, maxX);
+        int centerY = maxY / 2;
+        int centerX = maxX / 2;
+
+        move(48, centerX - textLength / 2); //двигаем курсор в центр нижнего блока
+
+
 
         attron(COLOR_PAIR(2)); // включение цветового паттерна
 
@@ -130,43 +214,56 @@ void gaming(std::string text, Player& player, Monster& monster) {
 
         attroff(COLOR_PAIR(2)); // его выключение
 
-        printw("%s\n", text.substr(textIndex, std::strlen(text.c_str())).c_str()); // вывод еше не введенных символов
-
-        printw("Your line: %s", letters.c_str()); // вывод введенных символов
-
-        if (mistake) { // при неверном вводе подсвечивает неверно введенный символ
+        if (mistake) {
             attron(COLOR_PAIR(1)); 
-            printw("%c", ch);
+            printw("%c", text[textIndex]); // при неверно введенном символе подсвечиваем текущий символ оригинальной строки
             attroff(COLOR_PAIR(1));
 
-            mistake = false;
-        } 
+            mistake = false; 
+        } else {
+            printw("%c", text[textIndex]); // иначе все как есть
+        }
 
-        // printw("Input: %s\n", input.c_str());
+        if (textIndex != textLength) {
+            printw("%s\n", text.substr(textIndex + 1, std::strlen(text.c_str())).c_str()); // вывод еше не введенных символов
+        }
 
-        // Handle other real-time tasks here
+        printSprite(monster); // печатает спрайт монстра
+
+        interface(player, monster); // печатает интерфейс
+
     }
 
-    s_Finished = true;
-    // countdown.join();
+    if (monster.time - timePassed < 1) { // проверяем, при каких условиях мы вышли из цикла
+        s_TimeIsOver = true; // если закончилось время, то устанавливаем соответствующий флаг
+    } else if (monster.healthPoints < 1) { // если закончилось здоровье монстра, то ...
+        monster.healthPoints = monster.healthMax;
+        player.healthPoints = player.healthMax;
+    }
     
 }
 
 
 int main() {
+    
 
     initscr(); // инициализирует библиотеку ncurses и подготавливает терминал к работе
     start_color(); // подключает поддержку цветов
+    raw(); // Disble line buffering
+    keypad(stdscr, TRUE); // Enable special keys (e.g., function keys)
+    noecho(); // Don't echo user input
+    curs_set(0); // Hide the cursor
+
+
     Player player;
     Monster monster;
+
     while (player.healthPoints > 0 and !s_TimeIsOver) {
         std::string text = getRandomLine(); // получаем строку для слепой печати
-        gaming(text, player, monster);
-        s_Finished = false;
+        gaming(text, player, monster); // запускаем игрульку
     }
 
     endwin(); // закрытие окошка
-
 
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,25 +1,120 @@
-#include <iostream>
-#include <chrono>
+#include <ncurses.h>
 
 int main() {
-    // Start the timer
-    auto start = std::chrono::steady_clock::now();
+    initscr(); // Initialize ncurses
+    noecho(); // Don't echo user input
+    cbreak(); // Disable line buffering
+    keypad(stdscr, TRUE); // Enable special keys (e.g., function keys)
+    curs_set(0); // Hide the cursor
+    start_color();
 
-    // Simulate some work or code execution
-    for (int i = 0; i < 100000; i++) {
-        for (int j = 0; j < 10000; j++) {
 
-        }
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK); // инициализируем первую цветовую палитру
+
+    int maxY, maxX;
+    getmaxyx(stdscr, maxY, maxX);
+
+    // Draw the top border
+    for (int x = 0; x < maxX; x++) {
+        mvaddch(0, x, ACS_HLINE); // Horizontal line character
     }
 
-    // Stop the timer
-    auto end = std::chrono::steady_clock::now();
+    // Draw the bottom border
+    for (int x = 0; x < maxX; x++) {
+        mvaddch(maxY - 1, x, ACS_HLINE); // Horizontal line character
+    }
 
-    // Calculate the elapsed time in seconds
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-    int i = elapsedTime.count();
+    // Draw the left and right borders
+    for (int y = 1; y < maxY - 1; y++) {
+        mvaddch(y, 0, ACS_VLINE); // Vertical line character
+        mvaddch(y, maxX - 1, ACS_VLINE); // Vertical line character
+    }
 
-    std::cout << "Elapsed time: " << elapsedTime.count() << " seconds" << std::endl << i;
+    // Draw a horizontal line 14 lines from the bottom
+    for (int x = 1; x < maxX - 1; x++) {
+        mvaddch(maxY - 15, x, ACS_HLINE); // Horizontal line character
+    }
+    for (int x = 1; x < maxX - 1; x++) {
+        mvaddch(11, x, ACS_HLINE); // Horizontal line character
+    }
+
+    int centerY = maxY / 2;
+    int centerX = maxX / 2;
+    const char* monsterArt[24] = {
+"                                              ,--,  ,.-.", 
+"                ,                   \\,       '-,-`,,-.' | ._", 
+"               /|           \\    ,   |\\         }  )/  / `-,',", 
+"               [ '          |\\  /|   | |        /  \\|  |/`  ,',", 
+"               | |       ,.`  `,` `, | |  _,...(   (      _,", 
+"               \\  \\  __ ,-` `  ,  , `/ |,'      Y     (   \\_L\\", 
+"                \\  \\_,``,   ` , ,  /  |         )         _,/", 
+"                 \\  '  `  ,_ _`_,-,<._.<        /         /", 
+"                  ', `>.,`  `  `   ,., |_      |         /", 
+"                    \\/`  `,   `   ,`  | /__,.-`    _,   `\\", 
+"                -,-..\\  _  \\  `  /  ,  / `._) _, -\\`       \\", 
+"                 \\_,,.) /\\    ` /  / ) (-,, ``    ,        |", 
+"                ,` )  | \\_\\       '-`  |  `(               \\", 
+"               /  /```(   , --, ,' \\   |`<`    ,            |", 
+"              /  /_,--`\\   <\\  V /> ,` )<_/)  | \\      _____)", 
+"        ,-, ,`   `   (_,\\ \\    |   /) / __/  /   `----`", 
+"       (-, \\           ) \\ ('_.-._)/ /,`    /", 
+"       | /  `          `/ \\\\ V   V, /`     /", 
+"    ,--,\\(        ,     <_/`\\\\     ||      /", 
+"   (   ,``-     \\/|         \\-A.A-`|     /", 
+"  ,>,_ )_,..(    )\\          -,,_-`  _--`", 
+" (_ \\|`   _,/_  /  \\_            ,--`", 
+"  \\( `   <.,../`     `-.._   _,-`", 
+"   `                  ",
+
+    };
+
+    attron(COLOR_PAIR(1)); // включение цветового паттерна
+
+
+    // Print the monster ASCII art line by line
+    for (int i = 0; i < 24; i++) {
+        mvprintw(centerY + i - 12, centerX - 30, "%s", monsterArt[i]);
+    }
+
+    attroff(COLOR_PAIR(1)); // его выключение
+
+    mvprintw(3, 118, "AIRAT");
+    mvprintw(4, 112, "HP: 91/100 ATK: 7");
+    mvprintw(5, 119, "YOU");
+    mvprintw(6, 102, "HP: 91/100 ATK: 1.1 LVL: 2 EXP: 15/30");
+    mvprintw(7, 112, "REMAINING TIME: 57");
+    refresh(); // Refresh the screen
+
+    int ch = getch(); // Wait for a key press
+
+    endwin(); // Terminate ncurses
 
     return 0;
 }
+
+/*
+                                              ,--,  ,.-.
+                ,                   \,       '-,-`,'-.' | ._
+               /|           \    ,   |\         }  )/  / `-,',
+               [ '          |\  /|   | |        /  \|  |/`  ,`
+               | |       ,.`  `,` `, | |  _,...(   (      _',
+               \  \  __ ,-` `  ,  , `/ |,'      Y     (   \_L\
+                \  \_\,``,   ` , ,  /  |         )         _,/
+                 \  '  `  ,_ _`_,-,<._.<        /         /
+                  ', `>.,`  `  `   ,., |_      |         /
+                    \/`  `,   `   ,`  | /__,.-`    _,   `\
+                -,-..\  _  \  `  /  ,  / `._) _,-\`       \
+                 \_,,.) /\    ` /  / ) (-,, ``    ,        |
+                ,` )  | \_\       '-`  |  `(               \
+               /  /```(   , --, ,' \   |`<`    ,            |
+              /  /_,--`\   <\  V /> ,` )<_/)  | \      _____)
+        ,-, ,`   `   (_,\ \    |   /) / __/  /   `----`
+       (-, \           ) \ ('_.-._)/ /,`    /
+       | /  `          `/ \\ V   V, /`     /
+    ,--\(        ,     <_/`\\     ||      /
+   (   ,``-     \/|         \-A.A-`|     /
+  ,>,_ )_,..(    )\          -,,_-`  _--`
+ (_ \|`   _,/_  /  \_            ,--`
+  \( `   <.,../`     `-.._   _,-`
+   `                      ```
+*/
